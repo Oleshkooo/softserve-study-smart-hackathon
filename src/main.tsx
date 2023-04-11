171e6e8b44c6777986f034cfebf252acd62c5ec4
Oleshkooo <olehchoma@gmail.com>
2023-04-11 11:45:34 +0300

    test merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from '@/App'
+import { disableConsole, googleAnalytics } from '@/utils'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// ? global styles
+// import '@/styles/globals.scss'
+
+// ? font awesome
+import('@/assets/icons/fontAwesome/all.min.css')
+import('@/assets/icons/fontAwesome/svg-with-js.min.css')
+
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+
+root.render(
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
 )
+
+if (import.meta.env.NODE_ENV === 'production') {
+    googleAnalytics()
+    disableReactDevTools()
+    disableConsole()
+}
